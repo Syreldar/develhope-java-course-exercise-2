c4a026d45a9376f4ab2ef1eb7d41b7cfdca7146e
Syreldar <enrico.drago3@gmail.com>
2023-07-11 12:36:30 +0200

    Upload exercise

--- PrintYourName.java
+++ PrintYourName.java
@@ -2,5 +2,7 @@ public class PrintYourName
 {
     public static void main(String[] args)
     {
+        String myName = "Enrico";
+        System.out.printf("Your name is: %s.%n", myName);
     }
 }
